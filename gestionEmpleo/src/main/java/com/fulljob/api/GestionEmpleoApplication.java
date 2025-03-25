760fb3080a1dceb74b854c7b10208c3d0d3d898a
Pablo <pabloprietolopez94@outlook.es>
2025-03-25 22:42:29 +0100

    Resubida de proyecto

---
+++ gestionEmpleo/src/main/java/com/fulljob/api/GestionEmpleoApplication.java
@@ -0,0 +1,13 @@
+package com.fulljob.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEmpleoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEmpleoApplication.class, args);
+	}
+
+}
