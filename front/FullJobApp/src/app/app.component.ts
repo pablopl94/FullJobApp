10f381de00116aceb5ab032588ca03ed91e8b571
Ruth Dupiereux <ruthdupiereux@gmail.com>
2025-04-01 19:05:15 +0200

    feat: Diseño HTML - Termino el diseño y el contenido de la parte pública

--- front/FullJobApp/src/app/app.component.ts
+++ front/FullJobApp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, FooterComponent]
 })
 export class AppComponent {}
 
