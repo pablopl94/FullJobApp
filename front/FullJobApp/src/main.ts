2723ec9d65dfa25403c5771bbc90580fb9857fa2
Pablo <pabloprietolopez94@outlook.es>
2025-04-20 16:14:40 +0200

    forzando despliegue

--- front/FullJobApp/src/main.ts
+++ front/FullJobApp/src/main.ts
@@ -2,16 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-import { enableProdMode } from '@angular/core'; // ✅ Esta es la correcta
+import { enableProdMode } from '@angular/core'; // ✅ esta es la buena
 
-// Solo activamos modo producción si está en entorno de producción
 if (environment.production) {
-  enableProdMode(); // ✅ Aquí se llama correctamente
+  enableProdMode(); 
 }
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-function enableProdMode() {
   throw new Error('Function not implemented.');
-}
+
 
