43977ca7819e0bbf3316f3fbe6fdfbd9bc7488d7
Pablo <pabloprietolopez94@outlook.es>
2025-04-20 15:39:25 +0200

    forzando despliegue

--- front/FullJobApp/src/main.ts
+++ front/FullJobApp/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+function enableProdMode() {
+  throw new Error('Function not implemented.');
+}
+
