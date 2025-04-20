764ea37157bfac033c941b6219521158ada2858d
Pablo <pabloprietolopez94@outlook.es>
2025-04-20 16:17:50 +0200

    forzando despliegue

--- front/FullJobApp/src/main.ts
+++ front/FullJobApp/src/main.ts
@@ -1,15 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
-import { enableProdMode } from '@angular/core'; // ✅ esta es la buena
-
-if (environment.production) {
-  enableProdMode(); 
-}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-  throw new Error('Function not implemented.');
-
-
